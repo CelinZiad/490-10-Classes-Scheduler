6dce93c499201b59023848bf05112f2629249dd3
Maïka Trépanier <maika.trepanier31@gmail.com>
2026-01-13 14:24:06 -0500

    Fix missing keys, remove comments

--- DatabaseScripts/DDL.sql
+++ DatabaseScripts/DDL.sql
@@ -1,13 +1,3 @@
--- Enum used in sequenceTerm and sequencePlan tables.
-
-CREATE TYPE entry_terms AS ENUM ('winter', 'fall', 'summer');
-
--- public.building definition
-
--- Drop table
-
--- DROP TABLE public.building;
-
 CREATE TABLE public.building (
 	campus varchar NOT NULL,
 	building varchar NOT NULL,
@@ -18,15 +8,8 @@ CREATE TABLE public.building (
 	CONSTRAINT building_pk PRIMARY KEY (campus, building)
 );
 
-
--- public."catalog" definition
-
--- Drop table
-
--- DROP TABLE public."catalog";
-
 CREATE TABLE public."catalog" (
-	id int4 NOT NULL,
+	id varchar NOT NULL,
 	title varchar NULL,
 	subject varchar NULL,
 	"catalog" varchar NULL,
@@ -37,13 +20,6 @@ CREATE TABLE public."catalog" (
 	CONSTRAINT catalog_pk PRIMARY KEY (id)
 );
 
-
--- public.facultydept definition
-
--- Drop table
-
--- DROP TABLE public.facultydept;
-
 CREATE TABLE public.facultydept (
 	facultycode varchar NOT NULL,
 	facultydescription varchar NOT NULL,
@@ -51,13 +27,6 @@ CREATE TABLE public.facultydept (
 	departmentdescription varchar NULL,
 	CONSTRAINT facultydept_pk PRIMARY KEY (facultycode, facultydescription)
 );
-
-
--- public.labrooms definition
-
--- Drop table
-
--- DROP TABLE public.labrooms;
 
 CREATE TABLE public.labrooms (
 	campus varchar NULL,
@@ -69,12 +38,41 @@ CREATE TABLE public.labrooms (
 	CONSTRAINT labrooms_unique UNIQUE (campus, building, room)
 );
 
+CREATE TABLE public.sequenceplan (
+	planid int4 NOT NULL,
+	planname varchar NOT NULL,
+	"program" varchar NULL,
+	entryterm public."entry_terms" NOT NULL,
+	"option" varchar NULL,
+	durationyears int4 NULL,
+	publishedon timestamp NULL,
+	CONSTRAINT sequenceplan_pk PRIMARY KEY (planid),
+	CONSTRAINT sequenceplan_unique UNIQUE (planname)
+);
 
--- public.scheduleterm definition
+CREATE TABLE public.sessions (
+	career varchar NULL,
+	termcode int4 NOT NULL,
+	termdescription varchar NULL,
+	sessioncode varchar NOT NULL,
+	sessiondescription varchar NULL,
+	sessionbegindate date NULL,
+	sessionenddate date NULL,
+	CONSTRAINT sessions_pk PRIMARY KEY (termcode, sessioncode)
+);
 
--- Drop table
+CREATE TABLE public."user" (
+	username varchar NOT NULL,
+	"password" varchar NOT NULL,
+	"name" varchar NULL,
+	CONSTRAINT user_pk PRIMARY KEY (username)
+);
 
--- DROP TABLE public.scheduleterm;
+CREATE TABLE public.courselabs (
+	courseid int4 NOT NULL,
+	labroomid int4 NULL,
+	CONSTRAINT courselabs_labrooms_fk FOREIGN KEY (labroomid) REFERENCES public.labrooms(labroomid) ON DELETE SET NULL ON UPDATE SET NULL
+);
 
 CREATE TABLE public.scheduleterm (
 	subject varchar NOT NULL,
@@ -105,15 +103,13 @@ CREATE TABLE public.scheduleterm (
 	fridays bool NULL,
 	saturdays bool NULL,
 	sundays bool NULL,
-	CONSTRAINT scheduleterm_pk PRIMARY KEY (subject, catalog, section, termcode, classnumber)
+	facultydescription varchar NULL,
+	CONSTRAINT scheduleterm_pk PRIMARY KEY (subject, catalog, section, termcode, classnumber),
+	CONSTRAINT scheduleterm_building_fk FOREIGN KEY (locationcode,buildingcode) REFERENCES public.building(campus,building) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT scheduleterm_catalog_fk FOREIGN KEY ("catalog") REFERENCES public."catalog"(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT scheduleterm_facultydept_fk FOREIGN KEY (facultycode,facultydescription) REFERENCES public.facultydept(facultycode,facultydescription) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT scheduleterm_sessions_fk FOREIGN KEY (termcode,"session") REFERENCES public.sessions(termcode,sessioncode) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-
--- public."section" definition
-
--- Drop table
-
--- DROP TABLE public."section";
 
 CREATE TABLE public."section" (
 	term int4 NOT NULL,
@@ -131,79 +127,21 @@ CREATE TABLE public."section" (
 	classwaitlistcapacity int4 NULL,
 	classwaitlisttotal int4 NULL,
 	"section" varchar NOT NULL,
-	CONSTRAINT section_pk PRIMARY KEY (term, subject, catalog, classnumber, section)
+	CONSTRAINT section_pk PRIMARY KEY (term, subject, catalog, classnumber, section),
+	CONSTRAINT section_catalog_fk FOREIGN KEY ("catalog") REFERENCES public."catalog"(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
--- public.sessions definition
-
--- Drop table
-
--- DROP TABLE public.sessions;
-
-CREATE TABLE public.sessions (
-	career varchar NULL,
-	termcode int4 NOT NULL,
-	termdescription varchar NULL,
-	sessioncode varchar NOT NULL,
-	sessiondescription varchar NULL,
-	sessionbegindate date NULL,
-	sessionenddate date NULL,
-	CONSTRAINT sessions_pk PRIMARY KEY (termcode, sessioncode)
-);
-
-
--- public."user" definition
-
--- Drop table
-
--- DROP TABLE public."user";
-
-CREATE TABLE public."user" (
-	username varchar NOT NULL,
-	"password" varchar NOT NULL,
-	"name" varchar NULL,
-	CONSTRAINT user_pk PRIMARY KEY (username)
-);
-
-
--- public.sequenceplan definition
-
--- Drop table
-
--- DROP TABLE public.sequenceplan;
-
-CREATE TABLE public.sequenceplan (
+CREATE TABLE public.sequenceterm (
+	sequencetermid int4 NOT NULL,
 	planid int4 NOT NULL,
-	planname varchar NOT NULL,
-	"program" varchar NULL,
-	entryterm public.entry_terms NOT NULL,
-	"option" varchar NULL,
-	durationyears int4 NULL,
-	publishedon timestamp NULL,
-	CONSTRAINT sequenceplan_pk PRIMARY KEY (planid),
-	CONSTRAINT sequenceplan_unique UNIQUE (planname)
+	yearnumber int4 NOT NULL,
+	season public."entry_terms" NOT NULL,
+	workterm bool DEFAULT false NOT NULL,
+	notes varchar NULL,
+	CONSTRAINT sequenceterm_pk PRIMARY KEY (sequencetermid),
+	CONSTRAINT sequenceterm_unique UNIQUE (planid, yearnumber, season, workterm),
+	CONSTRAINT sequenceterm_sequenceplan_fk FOREIGN KEY (planid) REFERENCES public.sequenceplan(planid) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-
--- public.courselabs definition
-
--- Drop table
-
--- DROP TABLE public.courselabs;
-
-CREATE TABLE public.courselabs (
-	courseid int4 NOT NULL,
-	labroomid int4 NULL,
-	CONSTRAINT courselabs_labrooms_fk FOREIGN KEY (labroomid) REFERENCES public.labrooms(labroomid) ON DELETE SET NULL ON UPDATE SET NULL
-);
-
-
--- public.studentschedulestudy definition
-
--- Drop table
-
--- DROP TABLE public.studentschedulestudy;
 
 CREATE TABLE public.studentschedulestudy (
 	studyid int4 NOT NULL,
@@ -214,32 +152,6 @@ CREATE TABLE public.studentschedulestudy (
 	CONSTRAINT studentschedulestudy_user_fk FOREIGN KEY ("owner") REFERENCES public."user"(username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
--- public.sequenceterm definition
-
--- Drop table
-
--- DROP TABLE public.sequenceterm;
-
-CREATE TABLE public.sequenceterm (
-	sequencetermid int4 NOT NULL,
-	planid int4 NOT NULL,
-	yearnumber int4 NOT NULL,
-	season public.entry_terms NOT NULL,
-	workterm bool DEFAULT false NOT NULL,
-	notes varchar NULL,
-	CONSTRAINT sequenceterm_pk PRIMARY KEY (sequencetermid),
-	CONSTRAINT sequenceterm_unique UNIQUE (planid, yearnumber, season, workterm),
-	CONSTRAINT sequenceterm_sequenceplan_fk FOREIGN KEY (planid) REFERENCES public.sequenceplan(planid) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-
--- public.sequencecourse definition
-
--- Drop table
-
--- DROP TABLE public.sequencecourse;
-
 CREATE TABLE public.sequencecourse (
 	sequencetermid int4 NOT NULL,
 	subject varchar NOT NULL,
@@ -247,15 +159,9 @@ CREATE TABLE public.sequencecourse (
 	"label" varchar NULL,
 	iselective bool NOT NULL,
 	CONSTRAINT sequencecourse_pk PRIMARY KEY (sequencetermid, subject, catalog),
+	CONSTRAINT sequencecourse_catalog_fk FOREIGN KEY ("catalog") REFERENCES public."catalog"(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT sequencecourse_sequenceterm_fk FOREIGN KEY (sequencetermid) REFERENCES public.sequenceterm(sequencetermid) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-
--- public.studentschedule definition
-
--- Drop table
-
--- DROP TABLE public.studentschedule;
 
 CREATE TABLE public.studentschedule (
 	studentscheduleid int4 NOT NULL,
@@ -266,13 +172,6 @@ CREATE TABLE public.studentschedule (
 	CONSTRAINT studentschedule_unique UNIQUE (schedulename, studyid),
 	CONSTRAINT studentschedule_studentschedulestudy_fk FOREIGN KEY (studyid) REFERENCES public.studentschedulestudy(studyid) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-
--- public.studentscheduleclass definition
-
--- Drop table
-
--- DROP TABLE public.studentscheduleclass;
 
 CREATE TABLE public.studentscheduleclass (
 	classentryid int4 NOT NULL,
