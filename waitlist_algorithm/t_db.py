da589a053947e95a93dd1ecef70106b534f5d581
Vincent Nguyen <Vincent7nguyen@gmail.com>
2026-02-12 15:13:46 -0500

    Database Connection test

--- waitlist_algorithm/t_db.py
+++ waitlist_algorithm/t_db.py
@@ -1,29 +1,20 @@
-import os
 from waitlist_algorithm.db import get_conn
 
-def main():
-    print("Starting DB check...")
-    print("PGHOST =", os.getenv("PGHOST"))
-    print("PGPORT =", os.getenv("PGPORT"))
-    print("PGDATABASE =", os.getenv("PGDATABASE"))
-    print("PGUSER =", os.getenv("PGUSER"))
-    # Don't print password
+query = """
+SELECT classnumber, subject, catalog, termcode,
+       classstarttime, classendtime,
+       mondays, tuesdays, wednesdays, thursdays, fridays
+FROM scheduleterm
+WHERE subject = 'COEN'
+LIMIT 5;
+"""
 
-    try:
-        print("Connecting...")
-        conn = get_conn()
-        print("Connected!")
 
-        with conn:
-            with conn.cursor() as cur:
-                print("Running query...")
-                cur.execute("SELECT 1;")
-                row = cur.fetchone()
-                print("Result:", row)
 
-        print("Done.")
-    except Exception as e:
-        print("ERROR:", repr(e))
+with get_conn() as conn:
+    with conn.cursor() as cur:
+        cur.execute(query)
+        rows= cur.fetchall()
 
-if __name__ == "__main__":
-    main()
+for row in rows:
+    print(row)
