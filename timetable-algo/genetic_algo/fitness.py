4fa33e02e856f4e9d4b525a9992e70b0d3b111b3
dpav341 <d.pavone@outlook.com>
2026-03-18 21:03:12 -0400

    Update fitness.py

--- timetable-algo/genetic_algo/fitness.py
+++ timetable-algo/genetic_algo/fitness.py
@@ -1,5 +1,6 @@
 # fitness.py
 from itertools import product
+from overlap_utils import times_overlap, get_course_by_code, has_valid_sequence_combination
 
 def calculate_variety_score(elements):
     """Calculate variety score for course elements (0-1, higher = more variety)."""
@@ -28,20 +29,6 @@ def calculate_variety_score(elements):
     return variety_score
 
 
-def times_overlap(element1, element2):
-    """Check if two course elements overlap in time."""
-    if element1 is None or element2 is None:
-        return False
-    
-    days1 = set(element1.day)
-    days2 = set(element2.day)
-    
-    if not days1.intersection(days2):
-        return False
-    
-    return element1.start < element2.end and element2.start < element1.end
-
-
 def count_lecture_conflicts(course):
     """Count conflicts between course lecture and its tutorials/labs."""
     conflicts = 0
@@ -60,88 +47,6 @@ def count_lecture_conflicts(course):
                 conflicts += 1
     
     return conflicts
-
-
-def get_course_by_code(schedule, course_code, class_nbr=None):
-    """Find a course in the schedule by subject+catalog_nbr and optionally class_nbr."""
-    subject = ''.join(c for c in course_code if c.isalpha())
-    catalog = ''.join(c for c in course_code if c.isdigit())
-    
-    for course in schedule:
-        if course.subject == subject and course.catalog_nbr == catalog:
-            if class_nbr is None or course.class_nbr == class_nbr:
-                return course
-    return None
-
-
-def has_valid_sequence_combination(schedule, sequence_courses):
-    """Check if there's at least one valid combination of tutorials/labs without overlaps."""
-    courses = []
-    for course_code in sequence_courses:
-        course = get_course_by_code(schedule, course_code)
-        if course is None:
-            return False
-        courses.append(course)
-    
-    all_tutorials = []
-    all_labs = []
-    
-    for course in courses:
-        if course.tutorial:
-            valid_tuts = [t for t in course.tutorial if t is not None]
-            if valid_tuts:
-                all_tutorials.append(valid_tuts)
-        
-        if course.lab:
-            valid_labs = [l for l in course.lab if l is not None]
-            if valid_labs:
-                all_labs.append(valid_labs)
-    
-    if not all_tutorials and not all_labs:
-        return True
-    
-    tut_combinations = list(product(*all_tutorials)) if all_tutorials else [[]]
-    lab_combinations = list(product(*all_labs)) if all_labs else [[]]
-    
-    for tut_combo in tut_combinations:
-        tut_has_overlap = False
-        for i, tut1 in enumerate(tut_combo):
-            for tut2 in tut_combo[i+1:]:
-                if times_overlap(tut1, tut2):
-                    tut_has_overlap = True
-                    break
-            if tut_has_overlap:
-                break
-        
-        if tut_has_overlap:
-            continue
-        
-        for lab_combo in lab_combinations:
-            lab_has_overlap = False
-            for i, lab1 in enumerate(lab_combo):
-                for lab2 in lab_combo[i+1:]:
-                    if times_overlap(lab1, lab2):
-                        lab_has_overlap = True
-                        break
-                if lab_has_overlap:
-                    break
-            
-            if lab_has_overlap:
-                continue
-            
-            tut_lab_overlap = False
-            for tut in tut_combo:
-                for lab in lab_combo:
-                    if times_overlap(tut, lab):
-                        tut_lab_overlap = True
-                        break
-                if tut_lab_overlap:
-                    break
-            
-            if not tut_lab_overlap:
-                return True
-    
-    return False
 
 
 def count_sequence_conflicts(schedule, core_sequences):
